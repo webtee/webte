767efa7db05b7d90f54780d4b40ae2e967c73823
eduardo <xmartinez@stuba.sk>
2018-12-30 18:11:42 +0100

    Third commit Edo

---
+++ myScript.js
@@ -0,0 +1,28 @@
+//3 Urovnove menu
+$(document).ready(function(){
+  $('.dropdown-submenu a.test').on("click", function(e){
+    $(this).next('ul').toggle();
+    e.stopPropagation();
+    e.preventDefault();
+  });
+});
+
+//Aktualny datum
+var datum = document.getElementById("datum");
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0!
+var yyyy = today.getFullYear();
+
+if(dd<10) {
+    dd = '0'+dd
+} 
+
+if(mm<10) {
+    mm = '0'+mm
+} 
+
+today = dd + '/' + mm + '/' + yyyy;
+datum.innerHTML = today;
+
+
